fce6d3abc9bbe148ac9e2970cd2dd4894dadeaab
LeoLunardon <115712958+LeoLunardon@users.noreply.github.com>
2023-10-04 17:56:34 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,7 @@ import "./index.css";
 
 //Configurando router
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/homePage.jsx";
-import ProductsPage from "./pages/productsPage.jsx";
-import SalePage from "./pages/salePage.jsx";
-import LastSales from "./pages/lastSales.jsx";
-import LoginPage from "./pages/loginPage.jsx";
-import DashboardPage from "./pages/dashboardPage.jsx";
+
 
 const router = createBrowserRouter([
   {
