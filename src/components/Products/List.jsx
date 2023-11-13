5ddbc56fab2abb1595ddbeb39fea3c26f89b71dc
LeoLunardon <115712958+LeoLunardon@users.noreply.github.com>
2023-11-13 11:55:50 -0300

    Update List.jsx

--- src/components/Products/List.jsx
+++ src/components/Products/List.jsx
@@ -4,6 +4,8 @@ import "./Products.css";
 import QuantityEdit from "./QuantityEdit";
 import Notification, { notify } from "../Notification/Notification";
 import Register from "./Register";
+import * as XLSX from "xlsx";
+import { format } from "date-fns";
 import UrlServer from "../../Services/UrlServer";
 
 const List = () => {
@@ -21,6 +23,27 @@ const List = () => {
         notify("Erro ao buscar produtos!", "error");
       });
   }, []);
+
+  const formatDataForExcel = (data) => {
+    const formattedData = data.map((product) => ({
+      Nome: product.name,
+      "Preço de compra(R$)": product.price,
+      "Preço de venda(R$)": product.salePrice,
+      Quantidade: product.quantity,
+    }));
+
+    const worksheet = XLSX.utils.json_to_sheet(formattedData, {
+      header: ["Nome", "Preço de compra(R$)", "Preço de venda(R$)", "Quantidade"],
+    });
+
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Produtos");
+
+    const excelBlob = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+    return new Blob([excelBlob], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+  };
 
   const handleEditQuantity = (productId) => {
     setSelectedProductId(productId);
@@ -90,8 +113,16 @@ const List = () => {
       <Notification />
       <div className="flex justify-between">
         <h1 className="text-3xl font-medium mt-10 mb-5">Estoque de Produtos</h1>
-        <div className="mt-10">
+       
+        <div className="mt-10 flex">
           <Register />
+        <a
+          href={URL.createObjectURL(formatDataForExcel(products))}
+          download={`estoque_${new Date().toLocaleDateString()}.xlsx`}
+          className="bg-green-500 inline-block mt-4 h-10 mx-2 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Download Tabela
+        </a>
         </div>
       </div>
       <div className="max-h-96 overflow-y-auto">
